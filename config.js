1f9d924476271d933449c279292fd0b0b458f522
seputarbokep99 <seputarbokep99@gmail.com>
2026-07-22 17:02:07 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// =========================================================
+// Config repo GitHub kamu. Sesuaikan kalau owner/nama repo beda.
+// =========================================================
+const GH_OWNER = "seputarbokep99";   // username GitHub kamu
+const GH_REPO = "seputarbokep";      // nama repo
+const GH_BRANCH = "main";            // nama branch (biasanya "main")
+const GH_DATA_PATH = "videos.json";  // file tempat data video disimpan
